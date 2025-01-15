30850875b736819586db76590801ec23135ed047
Mirae.JK <158263388+javeria-kamran@users.noreply.github.com>
2025-01-15 23:35:50 +0500

    update

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -7,11 +7,11 @@ const Footer = () => (
   <footer className="bg-gray-900 py-8 text-center text-white mt-8">
     <div className="container mx-auto">
       <p className="text-gray-400">
-        &copy; 2024 Space Explorer. Created by Muqaddas Fatima. All rights reserved.
+        &copy; 2025 Space Explorer. All rights reserved.
       </p>
       <div className="flex justify-center space-x-6 mt-4">
         <a
-          href="https://www.linkedin.com/in/muqaddas-fatima-6aaa0a2b3"
+          href="#"
           className="hover:text-blue-500"
           aria-label="LinkedIn"
           target="_blank"
@@ -20,7 +20,7 @@ const Footer = () => (
           <FaLinkedin size={24} />
         </a>
         <a
-          href="https://github.com/muqaddasfatima576"
+          href="#"
           className="hover:text-blue-500"
           aria-label="GitHub"
           target="_blank"
@@ -29,7 +29,7 @@ const Footer = () => (
           <FaGithub size={24} />
         </a>
         <a
-          href="https://www.instagram.com/muqaddas_fatima25"
+          href="#"
           className="hover:text-blue-500"
           aria-label="Instagram"
           target="_blank"
